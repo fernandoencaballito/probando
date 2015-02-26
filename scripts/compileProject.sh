c9eef80dc89aabcca96e4d1b7a4d0044253461d8
fernandoencaballito <fernando.en.caballito@gmail.com>
2015-02-26 11:49:07 -0300

    pepe

--- scripts/compileProject.sh
+++ scripts/compileProject.sh
@@ -2,4 +2,4 @@
 set -ev
 mvn clean package
 chmod 700 ./target/uber-tp-0.0.1-SNAPSHOT.jar
-sudo timeout 20s ./target/uber-tp-0.0.1-SNAPSHOT.jar localhost 110 localhost
+sudo timeout 20s java -jar ./target/uber-tp-0.0.1-SNAPSHOT.jar localhost 110 localhost
