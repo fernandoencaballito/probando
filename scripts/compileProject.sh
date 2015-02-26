5525d12d6a447d99629ed5631cad1eabd848cfa9
fernandoencaballito <fernando.en.caballito@gmail.com>
2015-02-26 16:20:50 -0300

    ahora andan los exit

--- scripts/compileProject.sh
+++ scripts/compileProject.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+cleanup(){
+previousExitCode=$?
+if [ $previousExitCode -eq 124 ]; then
+	 exit 0
+fi
+exit $previousExitCode
+}
+
+jarPath="./target/uber-tp-0.0.1-SNAPSHOT.jar"
 set -ev
 mvn clean package
-chmod 700 ./target/uber-tp-0.0.1-SNAPSHOT.jar
-sudo timeout 20s java -jar ./target/uber-tp-0.0.1-SNAPSHOT.jar localhost 110 localhost
-exit(0)
+chmod 700 $jarPath
+trap  "cleanup" EXIT
+sudo timeout 20s java -jar $jarPath localhost 110 localhost
+
