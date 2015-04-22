845a6ba9c1accf893335119383af622fd81c6f05
fbejaran <fbejaran@itba.edu.ar>
2015-04-22 09:24:11 -0300

    scripts y informe

--- scripts/mainScript.sh
+++ scripts/mainScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ls
-chmod 700 ./scripts/compileProject.sh
+chmod 700 ./scripts/*
 ./scripts/compileProject.sh
-chmod 700 ./scripts/smokeTest.sh
 ./scripts/smokeTest.sh
+./scripts/createReport.sh
