35148dbd8f4a8ddd0f36177fa3a4e6d69be43f83
fbejaran <fbejaran@itba.edu.ar>
2015-05-01 17:19:52 -0300

    autocorreccion de errores de ortografia en el informe

--- scripts/createReport.sh
+++ scripts/createReport.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd "docs/"
 pandoc ./drafts/Informe.md -o Informe.pdf --toc
+ aspell list --lang=es < ./drafts/Informe.md
 echo 'Creado el informe'
+
--- scripts/install/documentsDependencies.sh
+++ scripts/install/documentsDependencies.sh
@@ -2,3 +2,5 @@
 sudo apt-get install pandoc
 #sudo apt-get install texlive-fonts-recommended
 sudo apt-get install texlive
+sudo apt-get install aspell-es
+
