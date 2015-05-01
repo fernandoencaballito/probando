ea1e929da68d8931f28d8271664480debf33ad35
fbejaran <fbejaran@itba.edu.ar>
2015-05-01 17:31:20 -0300

    autocorreccion de errores de ortografia en el informe

--- scripts/smokeTest.sh
+++ scripts/smokeTest.sh
@@ -11,7 +11,6 @@ exit $previousExitCode
 
 jarPath="./target/uber-tp-0.0.1-SNAPSHOT.jar"
 set -ev
-mvn clean package
 chmod 700 $jarPath
 trap  "cleanup" EXIT
 timeout 20s java -jar $jarPath localhost 110 localhost
