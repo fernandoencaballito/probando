a0d849094038335183c87e3d8b9a79a511735c4a
fernandoencaballito <fbejaran@itba.edu.ar>
2015-05-01 17:49:24 -0300

    autocorreccion de errores de ortografia en el informe

--- scripts/createReport.sh
+++ scripts/createReport.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "docs/"
 pandoc ./drafts/Informe.md -o Informe.pdf --toc
+echo '________________________________________________________'
+echo 'Revisando errores de ortografía en el informe'
  aspell list --lang=es < ./drafts/Informe.md
-echo 'Creado el informe'
+echo '----Creado el informe'
 
